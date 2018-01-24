8f756d18f91be828e7da5f3b092b27cfedb77b3a
Ethan Neff <ethan.neff@eneff.com>
2018-01-23 23:16:51 -0800

    fix: added apollo libs

--- client/index.js
+++ client/index.js
@@ -1,8 +1,25 @@
+// libs
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
+
+// apollo
+const client = new ApolloClient({
+  link: new HttpLink(),
+  cache: new InMemoryCache({
+    dataIdFromObject: o => o.id
+  })
+});
 
 const Root = () => {
-  return <div>Auths Starter</div>;
+  return (
+    <ApolloProvider client={client}>
+      <div>Auths Starter</div>
+    </ApolloProvider>
+  );
 };
 
 ReactDOM.render(<Root />, document.querySelector("#root"));
